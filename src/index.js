c90233a737a4c96f05e99b8a57b2c2a8b3abb6a7
elena-vost <angara1298@gmail.com>
2021-02-15 17:47:45 +0100

    steps

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 import Foods from './Foods';
 import {choice, remove} from './Helpers';
+
+let fruit = choice(Foods);
+
+console.log("I'd like one ${fruit}, please.");
+console.log("Here you go: ${fruit}");
